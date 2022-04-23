2f3cc2d1445533d46e4b4a8d0be9d9c8616dc231
Iasmim Pereira <iaasmimcristinaa@gmail.com>
2022-04-23 16:48:38 -0300

    Tentativas de terminar a atividade

--- bootCampJava/src/desafios/Desafio02Intermediario.java
+++ bootCampJava/src/desafios/Desafio02Intermediario.java
@@ -6,7 +6,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
-public class Desafio04 {
+public class Desafio02Intermediario {
     public static void main(String[] args) {
 
 
