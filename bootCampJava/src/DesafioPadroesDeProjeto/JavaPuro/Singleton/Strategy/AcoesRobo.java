b7c722c427764cb4b9402f5fb28ae0c8bf65cde0
Iasmim Pereira <iaasmimcristinaa@gmail.com>
2022-05-13 11:54:17 -0300

    Implementação dos padrões de projeto com Java puro. Utilização de Strategy.

---
+++ bootCampJava/src/DesafioPadroesDeProjeto/JavaPuro/Singleton/Strategy/AcoesRobo.java
@@ -0,0 +1,6 @@
+package DesafioPadroesDeProjeto.JavaPuro.Singleton.Strategy;
+
+public interface AcoesRobo {
+    void mover();
+
+}
--- bootCampJava/src/DesafioPadroesDeProjeto/JavaPuro/Singleton/Strategy/Main.java
+++ bootCampJava/src/DesafioPadroesDeProjeto/JavaPuro/Singleton/Strategy/Main.java
@@ -1,4 +1,7 @@
 package DesafioPadroesDeProjeto.JavaPuro.Singleton.Strategy;
 
 public class Main {
+    public static void main(String[] args) {
+        //Strategy: simplifica a variação de algoritmos para a resolução de um problema.
+    }
 }
